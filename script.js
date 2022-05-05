e1fe21700b657213bd428642433077427fd179cf
Victor Lima Garcia <victorlimagarcia@gmail.com>
2022-05-05 17:23:10 -0300

    Hipótese resolução 3

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const loginEmail = document.getElementsByClassName('trybewarts-login')[0];
-const loginSenha = document.getElementsByClassName('trybewarts-login')[1];
+// const loginEmail = document.getElementsByClassName('trybewarts-login')[0];
+// const loginSenha = document.getElementsByClassName('trybewarts-login')[1];
+
+const test = document.getElementById('eMail1')
+const pass = document.getElementById('pass1').value
+
+
+if(test === 'tryber@test.com'){
+    console.log('Verdadeiro')
+}
