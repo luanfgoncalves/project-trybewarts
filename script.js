41c7ee4c2489c1218fc4e80055e7f1103c13f0fe
Victor Lima Garcia <victorlimagarcia@gmail.com>
2022-05-06 11:56:04 -0300

    Requisito 18 completo

---
+++ script.js
@@ -0,0 +1,10 @@
+//--------------Requisito 18---------------
+
+function check() {
+  const agree = document.querySelector('#agreement').checked;
+  const subimit = document.getElementById('submit-btn');
+
+  if (agree === true) {
+    subimit.removeAttribute('disabled');
+  }
+}
