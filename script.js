33b9556edbb7a59133fcb85152d5d27e8ff38cd7
luanfgoncalves <luanfgoncalves@outlook.com>
2022-05-06 12:59:18 -0300

    refatoração exercicio 3

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // --------------Requisito 3----------------
-window.onload = pegarValorDeInput()
-function pegarValorDeInput() {
+
+const loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', (event) => {
   const valorDoLogin = document.getElementById("eMail1").value;
   const valorDaSenha = document.getElementById("pass1").value;
-
+  console.log('clique reconhecido')
   if (valorDoLogin === "tryber@teste.com" && valorDaSenha === "123456") {
     alert("Olá, Tryber!");
-  } else {
-    alert("Email ou senha inválidos");
-  }
-}
+    console.log('Olá Tryber!');
+    } else {
+      alert("Email ou senha inválidos");
+      console.log('Email ou senha inválidos');
+    }
+})
 
 
 // --------------Requisito 18---------------
