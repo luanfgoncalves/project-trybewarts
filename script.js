cb8d83849b820445d728ad001e3eab65ea3dde36
Victor Lima Garcia <victorlimagarcia@gmail.com>
2022-05-06 12:29:46 -0300

    Continuação  do Requisito 3

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// --------------Requisito 3----------------
+window.onload = pegarValorDeInput()
+function pegarValorDeInput() {
+  const valorDoLogin = document.getElementById("eMail1").value;
+  const valorDaSenha = document.getElementById("pass1").value;
+
+  if (valorDoLogin === "tryber@teste.com" && valorDaSenha === "123456") {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos");
+  }
+}
+
+
 // --------------Requisito 18---------------
 
 function check() {
