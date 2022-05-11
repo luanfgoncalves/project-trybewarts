937c0b9ddd4e53738b5a1f86552ace383b13d778
luanfgoncalves <luanfgoncalves@outlook.com>
2022-05-10 22:48:51 -0300

    Adiciona melhorias ao CSS e HTML

--- script.js
+++ script.js
@@ -35,24 +35,3 @@ textarea.addEventListener('input', () => {
   const charNumber = maxText - texto;
   counter.textContent = charNumber;
 });
-
-// --------------Requisito 21---------------
-
-const nome = document.getElementById('input-name');
-const lastName = document.getElementById('input-last-name');
-const mail = document.getElementById('input-email');
-const casa1 = document.getElementById('gitnoria-house');
-const casa2 = document.getElementById('reactpuff-house');
-const casa3 = document.getElementById('corvinode-house');
-const casa4 = document.getElementById('pytherina-house');
-const family1 = document.getElementsByClassName('radio')[0];
-const family2 = document.getElementsByClassName('radio')[1];
-const family3 = document.getElementsByClassName('radio')[2];
-const learn1 = document.getElementsByClassName('subject')[0];
-const learn2 = document.getElementsByClassName('subject')[1];
-const learn3 = document.getElementsByClassName('subject')[2];
-const learn4 = document.getElementsByClassName('subject')[3];
-const learn5 = document.getElementsByClassName('subject')[4];
-const learn6 = document.getElementsByClassName('subject')[5];
-const evaluation = document.getElementsByClassName('radio2');
-
